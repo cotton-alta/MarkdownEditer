1348116a8761c43eb6a83ac882470ff955277586
cotton <chinoknct@gmail.com>
2020-09-20 23:29:58 +0900

    [add]ポップアップメニューの追加

--- client/src/components/ui/CreateButton.tsx
+++ client/src/components/ui/CreateButton.tsx
@@ -3,8 +3,16 @@ import React from "react";
 import "../../assets/style/button.scss";
 
 const CreateButton: React.FC = () => {
+  const createPage = () => {
+    // reducerでページ全体に対してのabsoluteをかけたポップアップメニューを出す
+    console.log("click!");
+  };
+  
   return (
-    <div className="c-button-wrapper">
+    <div 
+      className="c-button-wrapper"
+      onClick={createPage}
+    >
     </div>
   )
 };
